c56938d7c709009460fa61a1cc5a068d6f7e1010
Florian Marending <floriama@student.ethz.ch>
2018-11-02 14:23:09 +0100

    Fix fps flickering

--- src/ui.rs
+++ src/ui.rs
@@ -4,6 +4,7 @@ use nannou::ui::prelude::*;
 pub struct UserInterface {
     pub ui: Ui,
     fps_id: widget::Id,
+    fps_text: String,
     console_id: widget::Id,
     console_text: String,
     show_console: bool,
@@ -14,7 +15,7 @@ impl UserInterface {
     pub fn new(mut ui: Ui) -> Self {
         let fps_id = ui.generate_widget_id();
         let console_id = ui.generate_widget_id();
-        UserInterface { ui: ui, fps_id: fps_id, console_id: console_id, console_text: String::new(), show_console: false, last_update: 0.0 }
+        UserInterface { ui: ui, fps_id: fps_id, fps_text: String::new(), console_id: console_id, console_text: String::new(), show_console: false, last_update: 0.0 }
     }
 
     pub fn toggle_console(&mut self) {
@@ -30,15 +31,14 @@ impl UserInterface {
         if self.last_update > ui_update_interval {
             self.last_update = 0.0;
 
-            let fps = (1.0/dt).round().to_string();
-
-            widget::Text::new(&fps)
-                .right_justify()
-                .top_right_with_margin(5.0)
-                .color(ui::Color::Rgba(0.0, 0.0, 0.0, 1.0))
-                .set(self.fps_id, ui);
-
+            self.fps_text = (1.0/dt).round().to_string();
         }
+
+        widget::Text::new(&self.fps_text)
+            .right_justify()
+            .top_right_with_margin(5.0)
+            .color(ui::Color::Rgba(0.0, 0.0, 0.0, 1.0))
+            .set(self.fps_id, ui);
 
         if self.show_console {
             for text in widget::TextEdit::new(&self.console_text)

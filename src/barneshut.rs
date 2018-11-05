c5066ab7ee7e381a398f8004dc07987ae2992880
Florian Marending <marending.florian@gmail.com>
2018-11-05 18:14:44 +0100

    Cont. (WIP)

--- src/barneshut.rs
+++ src/barneshut.rs
@@ -1,41 +1,39 @@
 use nannou::prelude::*;
 
-struct Quadrants {
-    quadrants: (Box<BarnesHut>, Box<BarnesHut>, Box<BarnesHut>, Box<BarnesHut>),
-    top_left: Vector2,
-    bottom_right: Vector2,
-}
-
 enum Node {
-    Children(Quadrants),
+    Quadrants(Box<BarnesHut>, Box<BarnesHut>, Box<BarnesHut>, Box<BarnesHut>),
     Single(Vector2),
     Empty,
 }
 
 
 pub struct BarnesHut {
-    child: Node,
+    children: Node,
+    top_left: Vector2,
+    bottom_right: Vector2,
+
+    // Center of mass
+    com: Vector2,
+    // Number of points
+    n: usize,
 }
 
 impl BarnesHut {
     pub fn new(points: &[Vector2]) -> Self {
-        let mut tree = BarnesHut { child: Node::Empty };
+        let mut tree = BarnesHut { children: Node::Empty, top_left: Vector2::new(0.0, 0.0), bottom_right: Vector2::new(0.0, 0.0), com: Vector2::new(0.0, 0.0), n: points.len() };
+
+        let mut x_max = points[0].x;
+        let mut x_min = points[0].x;
+        let mut y_max = points[0].y;
+        let mut y_min = points[0].y;
 
         for p in points {
-            seep(&mut tree, *p);
+            x_max = p.x.max(x_max);
+            x_min = p.x.min(x_min);
+            y_max = p.y.max(y_max);
+            y_min = p.y.min(y_min);
         }
 
         tree
-    }
-}
-
-fn seep(tree: &mut BarnesHut, elem: Vector2) {
-    if let Node::Single(leaf) = tree.child {
-        let up = leaf.y.max(elem.y);
-        let down = leaf.y.min(elem.y);
-        let left = leaf.x.min(elem.x);
-        let right = leaf.x.max(elem.x);
-    } else {
-        seep(tree, elem);
     }
 }

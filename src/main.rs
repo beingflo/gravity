44743952a652fa0c125ef276be87521eb3a1206c
Florian Marending <floriama@student.ethz.ch>
2018-11-07 11:13:47 +0100

    Fix fps counter when <60

--- src/main.rs
+++ src/main.rs
@@ -48,9 +48,9 @@ fn event(_: &App, mut model: Model, event: Event) -> Model {
             let dt = update.since_last.secs() as f32;
 
             // Don't let slow framerate mess up the simulation
-            let dt = dt.min(1.0 / 60.0);
+            let dt_simulation = dt.min(1.0 / 60.0);
 
-            model.arena.step(dt);
+            model.arena.step(dt_simulation);
             model.arena.update();
             model.ui.update(dt);
         },
